04cb6fb198db33255e42845529d93fc461092f56
shubham3619 <120643298+shubham3619@users.noreply.github.com>
2024-05-03 02:55:28 +0000

    commented

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+  setInterval(() => {
+    setCount(count + 1);
+  }, 1000);
+  
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>You've been on this page for {count} seconds.</p>
     </div>
   )
 }
